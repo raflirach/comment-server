84b2ec789fe427186a85934a1eaadb8bc98fc466
raflirach <rafli060395@gmail.com>
2021-03-19 07:30:26 +0700

    add: app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+
+const express = require('express')
+const { connect } = require('./config/mongodb')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+connect().then(() => {
+  const router = require('./routes')
+  const errorHandler = require('./middlewares/errorHandler')
+
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  
+  app.use('/', router)
+  app.use(errorHandler)
+  app.listen(PORT, () => {
+    console.log(`App listen to port : ${PORT}`);
+  })
+})
