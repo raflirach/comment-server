00090a27091bab37134159d47470b0827c24a411
raflirach <rafli060395@gmail.com>
2021-03-19 09:02:00 +0700

    add: cors

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 connect().then(() => {
+  const cors = require('cors')
   const router = require('./routes')
   const errorHandler = require('./middlewares/errorHandler')
+
+  app.use(cors())
 
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
