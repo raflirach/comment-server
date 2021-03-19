b078cb5f5e0ffe50b40558c18108f7b9ea14e493
raflirach <rafli060395@gmail.com>
2021-03-19 07:28:07 +0700

    add: database config

---
+++ config/mongodb.js
@@ -0,0 +1,29 @@
+
+const { MongoClient } = require('mongodb')
+
+let database = null
+
+async function connect() {
+  try {
+    const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.si30c.mongodb.net/comments?retryWrites=true&w=majority`
+    const client = new MongoClient(uri, { useUnifiedTopology: true })
+    
+    await client.connect()
+
+    const db = client.db(`comments`)
+
+    db
+
+    database = db
+
+  } catch (err) {
+    next(err);
+  }
+}
+
+const getDatabase = () => database
+
+module.exports = {
+  connect,
+  getDatabase
+}
