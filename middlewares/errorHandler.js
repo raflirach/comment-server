67a1edd6b5d4bbeb8303981a38aa781e0a42b2d2
raflirach <rafli060395@gmail.com>
2021-03-20 10:22:32 +0700

    edit: error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,17 @@
 const errorHandler = (err, req, res, next) => {
-  console.log(err);
   switch (err.name) {
+    case "ErrorValidation": 
+      res.status(400).json({
+        status: 'error',
+        errors: err.errors
+      })
+      break;
+    case "ErrorNotFound": 
+      res.status(404).json({
+        status: 'error',
+        errors: 'data not found'
+      })
+      break;
     default:
       res.status(500).json({
         status: 'error',
